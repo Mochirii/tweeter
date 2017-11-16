9d5c2ba152dc1858b908a873d80124973d793c8f
Mel Smith <melpcsmith@gmail.com>
2017-11-16 23:13:41 +0000

    set up tweeter, added new tweet form container and tweet container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  var text_max = 140;
+  $('#textarea_feedback').html(text_max + ' characters remaining');
+
+  var $textCounter = $('.counter');
+
+  var $counter = $(".counter");
+
+  $('textarea').keyup(function() {
+      var text_length = $(this).val().length;
+      var text_remaining = text_max - text_length;
+
+      $textCounter.text(text_remaining);
+
+      if (text_length <= text_max) {
+        return $counter.css("color", "#0A3517");
+      
+      }
+        
+      $counter.css("color", "#ff0000");;
+    });
+  });
